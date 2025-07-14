fa33f2873353baef72fe4d55954f5f10590268e0
atha <144105765+Athalhm@users.noreply.github.com>
2025-07-14 15:39:39 +0700

    update2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,9 @@
-module.exports = {
-  content: {
-    files: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./resources/views/**/*.blade.php",
-    ],
-    safelist: [
-      'bg-red-100',
-      'bg-red-500',
-      'bg-blue-500',
-      'text-gray-800',
-      'min-h-screen',
-      'flex',
-      'flex-col'
-    ],
-  },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
